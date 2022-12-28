57cd535e5a25ca54acbd5c24f53a55fa7882506a
jojongguk <cLazyZombie@gmail.com>
2022-12-28 14:38:20 +0900

    endian

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+fn main() {}
+
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn test_endian() {
+        let v: u32 = 0x01020304;
+
+        let be = v.to_be_bytes();
+        assert_eq!(be, [0x01, 0x02, 0x03, 0x04]);
+
+        let le = v.to_le_bytes();
+        assert_eq!(le, [0x04, 0x03, 0x02, 0x01]);
+
+        let ne = v.to_ne_bytes();
+        println!("ne: {:?}", ne);
+    }
 }
